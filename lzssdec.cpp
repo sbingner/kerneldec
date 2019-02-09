2e8b54510e45b1ed61e80f49e94914035c2fc4de
Sam Bingner <sam@bingner.com>
2019-02-08 14:22:33 -1000

    Cleanup whitespace

--- lzssdec.cpp
+++ lzssdec.cpp
@@ -14,129 +14,129 @@
 //
 int g_debug= 0;
 
-    lzssdecompress::lzssdecompress()
-    {
-        _maxmatch= 18;  // 4 bit size + threshold 
-        _dictsize= 4096; // 12 bit size
-        _copythreshold= 3; // 0 == copy 3 bytes
-        _dict= new uint8_t[_dictsize+_maxmatch-1];
+lzssdecompress::lzssdecompress()
+{
+    _maxmatch= 18;  // 4 bit size + threshold 
+    _dictsize= 4096; // 12 bit size
+    _copythreshold= 3; // 0 == copy 3 bytes
+    _dict= new uint8_t[_dictsize+_maxmatch-1];
 
-        reset();
-    }
-    lzssdecompress::~lzssdecompress()
-    {
-        delete[] _dict;
-        _dict= 0; _dictsize= 0;
-    }
-    void lzssdecompress::reset()
-    {
-        _state=EXPECTINGFLAG;
-        _flags= 0; _bitnr= 0;
-        _src=_srcend=_dst=_dstend=0;
-        memset(_dict, ' ', _dictsize+_maxmatch-1);
-        _dictptr= _dictsize-_maxmatch;
-        _inputoffset= 0;
-        _outputoffset= 0;
-        _firstbyte= 0;
-        _copyptr= 0;
-        _copycount= 0;
-    }
-    void lzssdecompress::decompress(uint8_t *dst, uint32_t dstlen, uint32_t *pdstused, uint8_t *src, uint32_t srclen, uint32_t *psrcused)
-    {
-        _src= src;  _srcend= src+srclen;
-        _dst= dst;  _dstend= dst+dstlen;
+    reset();
+}
+lzssdecompress::~lzssdecompress()
+{
+    delete[] _dict;
+    _dict= 0; _dictsize= 0;
+}
+void lzssdecompress::reset()
+{
+    _state=EXPECTINGFLAG;
+    _flags= 0; _bitnr= 0;
+    _src=_srcend=_dst=_dstend=0;
+    memset(_dict, ' ', _dictsize+_maxmatch-1);
+    _dictptr= _dictsize-_maxmatch;
+    _inputoffset= 0;
+    _outputoffset= 0;
+    _firstbyte= 0;
+    _copyptr= 0;
+    _copycount= 0;
+}
+void lzssdecompress::decompress(uint8_t *dst, uint32_t dstlen, uint32_t *pdstused, uint8_t *src, uint32_t srclen, uint32_t *psrcused)
+{
+    _src= src;  _srcend= src+srclen;
+    _dst= dst;  _dstend= dst+dstlen;
 
-        while (_src<_srcend && _dst<_dstend)
+    while (_src<_srcend && _dst<_dstend)
+    {
+        switch(_state)
         {
-            switch(_state)
-            {
-                case EXPECTINGFLAG:
-                    if (g_debug) fprintf(stderr, "%08x,%08x: flag: %02x\n", _inputoffset, _outputoffset, *_src);
-                    _flags= *_src++;
+            case EXPECTINGFLAG:
+                if (g_debug) fprintf(stderr, "%08x,%08x: flag: %02x\n", _inputoffset, _outputoffset, *_src);
+                _flags= *_src++;
+                _inputoffset++;
+                _bitnr= 0;
+                _state= PROCESSFLAGBIT;
+                break;
+            case PROCESSFLAGBIT:
+                if (_flags&1) {
+                    if (g_debug) fprintf(stderr, "%08x,%08x: bit%d: %03x copybyte %02x\n", _inputoffset, _outputoffset, _bitnr, _dictptr, *_src);
+                    addtodict(*_dst++ = *_src++);
                     _inputoffset++;
-                    _bitnr= 0;
-                    _state= PROCESSFLAGBIT;
-                    break;
-                case PROCESSFLAGBIT:
-                    if (_flags&1) {
-                        if (g_debug) fprintf(stderr, "%08x,%08x: bit%d: %03x copybyte %02x\n", _inputoffset, _outputoffset, _bitnr, _dictptr, *_src);
-                        addtodict(*_dst++ = *_src++);
-                        _inputoffset++;
-                        _outputoffset++;
-                        nextflagbit();
-                    }
-                    else {
-                        _firstbyte= *_src++;
-                        _inputoffset++;
-                        _state= EXPECTING2NDBYTE;
-                    }
-                    break;
-                case EXPECTING2NDBYTE:
-                    {
+                    _outputoffset++;
+                    nextflagbit();
+                }
+                else {
+                    _firstbyte= *_src++;
+                    _inputoffset++;
+                    _state= EXPECTING2NDBYTE;
+                }
+                break;
+            case EXPECTING2NDBYTE:
+                {
                     uint8_t secondbyte= *_src++;
                     _inputoffset++;
                     setcounter(_firstbyte, secondbyte);
                     if (g_debug) fprintf(stderr, "%08x,%08x: bit%d: %03x %02x %02x : copy %d bytes from %03x", _inputoffset-2, _outputoffset, _bitnr, _dictptr, _firstbyte, secondbyte, _copycount, _copyptr);
                     if (g_debug) dumpcopydata();
                     _state= COPYFROMDICT;
-                    }
-                    break;
-                case COPYFROMDICT:
-                    copyfromdict();
-                    break;
-            }
+                }
+                break;
+            case COPYFROMDICT:
+                copyfromdict();
+                break;
         }
-        if (g_debug) fprintf(stderr, "decompress state= %d, copy: 0x%x, 0x%x\n", _state, _copyptr, _copycount);
-        if (pdstused) *pdstused= _dst-dst;
-        if (psrcused) *psrcused= _src-src;
     }
-    void lzssdecompress::flush(uint8_t *dst, uint32_t dstlen, uint32_t *pdstused)
-    {
-        if (g_debug) fprintf(stderr, "flash before state= %d, copy: 0x%x, 0x%x\n", _state, _copyptr, _copycount);
-        _src= _srcend= NULL;
-        _dst= dst;  _dstend= dst+dstlen;
+    if (g_debug) fprintf(stderr, "decompress state= %d, copy: 0x%x, 0x%x\n", _state, _copyptr, _copycount);
+    if (pdstused) *pdstused= _dst-dst;
+    if (psrcused) *psrcused= _src-src;
+}
+void lzssdecompress::flush(uint8_t *dst, uint32_t dstlen, uint32_t *pdstused)
+{
+    if (g_debug) fprintf(stderr, "flash before state= %d, copy: 0x%x, 0x%x\n", _state, _copyptr, _copycount);
+    _src= _srcend= NULL;
+    _dst= dst;  _dstend= dst+dstlen;
 
-        if (_state==COPYFROMDICT)
-            copyfromdict();
+    if (_state==COPYFROMDICT)
+        copyfromdict();
 
-        if (pdstused) *pdstused= _dst-dst;
-        if (g_debug) fprintf(stderr, "flash after state= %d, copy: 0x%x, 0x%x\n", _state, _copyptr, _copycount);
-    }
-    void lzssdecompress::copyfromdict()
+    if (pdstused) *pdstused= _dst-dst;
+    if (g_debug) fprintf(stderr, "flash after state= %d, copy: 0x%x, 0x%x\n", _state, _copyptr, _copycount);
+}
+void lzssdecompress::copyfromdict()
+{
+    while (_dst<_dstend && _copycount)
     {
-        while (_dst<_dstend && _copycount)
-        {
-            addtodict(*_dst++ = _dict[_copyptr++]);
-            _outputoffset++;
-            _copycount--;
-            _copyptr= _copyptr&(_dictsize-1);
-        }
-        if (_copycount==0)
-            nextflagbit();
+        addtodict(*_dst++ = _dict[_copyptr++]);
+        _outputoffset++;
+        _copycount--;
+        _copyptr= _copyptr&(_dictsize-1);
     }
-    void lzssdecompress::dumpcopydata()
-    {
-        // note: we are printing incorrect data, if _copyptr == _dictptr-1
-        for (int i=0 ; i<_copycount ; i++)
-            fprintf(stderr, " %02x", _dict[(_copyptr+i)&(_dictsize-1)]);
-        fprintf(stderr, "\n");
-    }
-    void lzssdecompress::addtodict(uint8_t c)
-    {
-        _dict[_dictptr++]= c;
-        _dictptr = _dictptr&(_dictsize-1);
-    }
-    void lzssdecompress::nextflagbit()
-    {
-        _bitnr++;
-        _flags>>=1;
-        _state = _bitnr==8 ? EXPECTINGFLAG : PROCESSFLAGBIT;
-    }
-    void lzssdecompress::setcounter(uint8_t first, uint8_t second)
-    {
-        _copyptr= first | ((second&0xf0)<<4);
-        _copycount= _copythreshold + (second&0xf);
-    }
+    if (_copycount==0)
+        nextflagbit();
+}
+void lzssdecompress::dumpcopydata()
+{
+    // note: we are printing incorrect data, if _copyptr == _dictptr-1
+    for (int i=0 ; i<_copycount ; i++)
+        fprintf(stderr, " %02x", _dict[(_copyptr+i)&(_dictsize-1)]);
+    fprintf(stderr, "\n");
+}
+void lzssdecompress::addtodict(uint8_t c)
+{
+    _dict[_dictptr++]= c;
+    _dictptr = _dictptr&(_dictsize-1);
+}
+void lzssdecompress::nextflagbit()
+{
+    _bitnr++;
+    _flags>>=1;
+    _state = _bitnr==8 ? EXPECTINGFLAG : PROCESSFLAGBIT;
+}
+void lzssdecompress::setcounter(uint8_t first, uint8_t second)
+{
+    _copyptr= first | ((second&0xf0)<<4);
+    _copycount= _copythreshold + (second&0xf);
+}
 
 #ifdef HAVE_MAIN
 void usage()
@@ -145,9 +145,6 @@ void usage()
 }
 int main(int argc,char**argv)
 {
-//    _setmode(fileno(stdin),O_BINARY);
-//    _setmode(fileno(stdout),O_BINARY);
-
 #define HANDLEULOPTION(var, type) (argv[i][2] ? var= (type)strtoul(argv[i]+2, 0, 0) : i+1<argc ? var= (type)strtoul(argv[++i], 0, 0) : 0)
 
     uint32_t skipbytes=0;
